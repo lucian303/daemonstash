3f8a1a51de3f431c1fe441ca6a148145902c9238
Lucian Hontau <lucian303@gmail.com>
2012-12-08 03:36:05 -0800

    commented out autoload. need to deploy on php5.3

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 // Change working directory to base directory, then we can use getcwd()
 chdir(dirname(__DIR__));
 
-require getcwd() . '/vendor/autoload.php';
+//require getcwd() . '/vendor/autoload.php';
 
 require_once getcwd() . '/src/DaemonDash/Directory/DirectoryPrinter.php';
 // look for uploads directory out of the web root
